abbde31730962dec14d19988460b42faa7aa09c8
chxuru <justinsp226@gmail.com>
2024-11-11 22:18:17 -0800

    Update command.go

--- command.go
+++ command.go
@@ -1,3 +1,9 @@
+package main
+import (
+    "github.com/bwmarrin/discordgo"
+    "log"
+)
+
 func registerCommands(discord *discordgo.Session) {
     commands := []discordgo.ApplicationCommand{
         {
