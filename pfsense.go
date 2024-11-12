49d6a252b06d45974b94b16a7bffa60566c5d520
chxuru <justinsp226@gmail.com>
2024-11-11 23:40:45 -0800

    Update pfsense.go

--- pfsense.go
+++ pfsense.go
@@ -205,3 +205,24 @@ func deleteUser(client *http.Client, ids []string) error {
 
     return nil
 }
+
+func terminateSession(client *http.Client, port, clientIPPort string) error {
+    terminateURL := fmt.Sprintf("%s/status_openvpn.php?action=kill&port=%s&remipp=%s&client_id=", BaseURL, port, clientIPPort)
+
+    req, err := http.NewRequest("GET", terminateURL, nil)
+    if err != nil {
+        return fmt.Errorf("failed to create terminate request: %w", err)
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("failed to execute terminate request: %w", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return fmt.Errorf("terminate request failed with status: %s", resp.Status)
+    }
+
+    return nil
+}
