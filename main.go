1764ff13791bca8094dd12c1d342afa726f18c6f
chxuru <justinsp226@gmail.com>
2024-11-15 22:38:03 -0800

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ func main() {
     }
     defer discord.Close()
 
-    discord.AddHandler(handleInteraction)
-
     err = discord.Open()
     if err != nil {
         log.Fatalf("Failed to open Discord session: %v", err)
@@ -21,21 +19,4 @@ func main() {
 
     log.Println("Bot is running!")
     select {}
-}
-
-func handleInteraction(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if i.Type == discordgo.InteractionApplicationCommand {
-        guildID := i.GuildID
-        log.Printf("Command executed in Guild ID: %s", guildID)
-
-        err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "This command is being executed in your server!",
-            },
-        })
-        if err != nil {
-            log.Printf("Error responding to interaction: %v", err)
-        }
-    }
 }
